e9a094e1e9e51856cb05301dab3f451c53131eab
caojiaju-2017 <jiaju_cao@hotmail.com>
2018-08-01 21:22:48 +0800

    12
    323

--- NAKHome/Nak/Api/WebCenterApi.py
+++ NAKHome/Nak/Api/WebCenterApi.py
@@ -8,7 +8,9 @@ class WebCenterApi(object):
     @csrf_exempt
     def openHome(request):
         renterDict = {}
-        return render(request, 'index.html',renterDict )
+        # return render(request, 'service_1.html',renterDict )
+
+        return render(request, 'index.html', renterDict)
 
     @staticmethod
     @csrf_exempt
@@ -59,42 +61,186 @@ class WebCenterApi(object):
     # ###############################################################################################################
     @staticmethod
     @csrf_exempt
+    def openNews102624(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102624.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102627(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102627.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102630(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102630.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102632(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102632.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102635(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102635.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102637(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102637.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102640(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102640.html',renterDict )
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102643(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102643.html',renterDict )
+
+
+    @staticmethod
+    @csrf_exempt
     def openNews102645(request):
         renterDict = {}
-        return render(request, 'newsDetail_102645.html',renterDict )
+        return render(request, 'news/newsDetail_102645.html',renterDict )
 
     @staticmethod
     @csrf_exempt
     def openNews102648(request):
         renterDict = {}
-        return render(request, 'newsDetail_102648.html',renterDict )
+        return render(request, 'news/newsDetail_102648.html',renterDict )
 
     @staticmethod
     @csrf_exempt
     def openNews102651(request):
         renterDict = {}
-        return render(request, 'newsDetail_102651.html', renterDict)
+        return render(request, 'news/newsDetail_102651.html', renterDict)
 
     @staticmethod
     @csrf_exempt
     def openNews102653(request):
         renterDict = {}
-        return render(request, 'newsDetail_102653.html', renterDict)
+        return render(request, 'news/newsDetail_102653.html', renterDict)
 
     @staticmethod
     @csrf_exempt
     def openNews102656(request):
         renterDict = {}
-        return render(request, 'newsDetail_102656.html', renterDict)
+        return render(request, 'news/newsDetail_102656.html', renterDict)
 
     @staticmethod
     @csrf_exempt
     def openNews102659(request):
         renterDict = {}
-        return render(request, 'newsDetail_102659.html', renterDict)
+        return render(request, 'news/newsDetail_102659.html', renterDict)
 
     @staticmethod
     @csrf_exempt
     def openNews102662(request):
         renterDict = {}
-        return render(request, 'newsDetail_102662.html', renterDict)
+        return render(request, 'news/newsDetail_102662.html', renterDict)
+
+    # #####################################################
+    @staticmethod
+    @csrf_exempt
+    def openNews102665(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102665.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102672(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102672.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102675(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102675.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102678(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102678.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102682(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102682.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102685(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102685.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102688(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102688.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102690(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102690.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews102694(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_102694.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103736(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103736.html', renterDict)
+
+
+
+
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103740(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103740.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103744(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103744.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103746(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103746.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103750(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103750.html', renterDict)
+
+    @staticmethod
+    @csrf_exempt
+    def openNews103903(request):
+        renterDict = {}
+        return render(request, 'news/newsDetail_103903.html', renterDict)
--- NAKHome/NakServer/urls.py
+++ NAKHome/NakServer/urls.py
@@ -37,7 +37,14 @@ urlpatterns = [
     url(r'^service.html', WebCenterApi.openService),
     url(r'^footer.html', WebCenterApi.openFooter),
 
-
+    url(r'^newsDetail_102624.html', WebCenterApi.openNews102624),
+    url(r'^newsDetail_102627.html', WebCenterApi.openNews102627),
+    url(r'^newsDetail_102630.html', WebCenterApi.openNews102630),
+    url(r'^newsDetail_102632.html', WebCenterApi.openNews102632),
+    url(r'^newsDetail_102635.html', WebCenterApi.openNews102635),
+    url(r'^newsDetail_102637.html', WebCenterApi.openNews102637),
+    url(r'^newsDetail_102640.html', WebCenterApi.openNews102640),
+    url(r'^newsDetail_102643.html', WebCenterApi.openNews102643),
     url(r'^newsDetail_102645.html', WebCenterApi.openNews102645),
     url(r'^newsDetail_102648.html', WebCenterApi.openNews102648),
     url(r'^newsDetail_102651.html', WebCenterApi.openNews102651),
@@ -45,6 +52,20 @@ urlpatterns = [
     url(r'^newsDetail_102656.html', WebCenterApi.openNews102656),
     url(r'^newsDetail_102659.html', WebCenterApi.openNews102659),
     url(r'^newsDetail_102662.html', WebCenterApi.openNews102662),
-
+    url(r'^newsDetail_102665.html', WebCenterApi.openNews102665),
+    url(r'^newsDetail_102672.html', WebCenterApi.openNews102672),
+    url(r'^newsDetail_102675.html', WebCenterApi.openNews102675),
+    url(r'^newsDetail_102678.html', WebCenterApi.openNews102678),
+    url(r'^newsDetail_102682.html', WebCenterApi.openNews102682),
+    url(r'^newsDetail_102685.html', WebCenterApi.openNews102685),
+    url(r'^newsDetail_102688.html', WebCenterApi.openNews102688),
+    url(r'^newsDetail_102690.html', WebCenterApi.openNews102690),
+    url(r'^newsDetail_102694.html', WebCenterApi.openNews102694),
+    url(r'^newsDetail_103736.html', WebCenterApi.openNews103736),
+    url(r'^newsDetail_103740.html', WebCenterApi.openNews103740),
+    url(r'^newsDetail_103744.html', WebCenterApi.openNews103744),
+    url(r'^newsDetail_103746.html', WebCenterApi.openNews103746),
+    url(r'^newsDetail_103750.html', WebCenterApi.openNews103750),
+    url(r'^newsDetail_103903.html', WebCenterApi.openNews103903),
 
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
