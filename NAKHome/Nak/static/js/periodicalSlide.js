216b4961bca9e77cd4e0cf2c4bf5dd19736bab8f
caojiaju-2017 <jiaju_cao@hotmail.com>
2018-08-02 05:33:04 +0800

    12
    21

--- NAKHome/Nak/static/js/periodicalSlide.js
+++ NAKHome/Nak/static/js/periodicalSlide.js
@@ -6,19 +6,19 @@ var listjsonUser = [
     {pdfUrl:"",src:"images/periodical/201401.jpg",text:"《NAK科技》2014年01期"},
 	{pdfUrl:"",src:"images/periodical/201402.jpg",text:"《NAK科技》2014年02期"},
 	{pdfUrl:"",src:"images/periodical/201403.jpg",text:"《NAK科技》2014年03期"},
-	{pdfUrl:"",src:"images/periodical/201301.jpg",text:"《汉森乐园》2013年01期"},
-	{pdfUrl:"",src:"images/periodical/201302.jpg",text:"《汉森乐园》2013年02期"},
-	{pdfUrl:"",src:"images/periodical/201303.jpg",text:"《汉森乐园》2013年03期"},
-	{pdfUrl:"",src:"images/periodical/201304.jpg",text:"《汉森乐园》2013年04期"},
-	{pdfUrl:"",src:"images/periodical/201305.jpg",text:"《汉森乐园》2013年05期"},
-	{pdfUrl:"",src:"images/periodical/201306.jpg",text:"《汉森乐园》2013年06期"},
-	{pdfUrl:"",src:"images/periodical/201307.jpg",text:"《汉森乐园》2013年07期"},
-	{pdfUrl:"",src:"images/periodical/201308.jpg",text:"《汉森乐园》2013年08期"},
-	{pdfUrl:"",src:"images/periodical/201309.jpg",text:"《汉森乐园》2013年09期"},
-	{pdfUrl:"",src:"images/periodical/201310.jpg",text:"《汉森乐园》2013年10期"},
-	{pdfUrl:"",src:"images/periodical/201311.jpg",text:"《汉森乐园》2013年11期"},
-	{pdfUrl:"",src:"images/periodical/201312.jpg",text:"《汉森乐园》2013年12期"},
-	{pdfUrl:"",src:"images/periodical/201313.jpg",text:"《汉森乐园》2013年13期"}
+	{pdfUrl:"",src:"images/periodical/201301.jpg",text:"《NAK乐园》2013年01期"},
+	{pdfUrl:"",src:"images/periodical/201302.jpg",text:"《NAK乐园》2013年02期"},
+	{pdfUrl:"",src:"images/periodical/201303.jpg",text:"《NAK乐园》2013年03期"},
+	{pdfUrl:"",src:"images/periodical/201304.jpg",text:"《NAK乐园》2013年04期"},
+	{pdfUrl:"",src:"images/periodical/201305.jpg",text:"《NAK乐园》2013年05期"},
+	{pdfUrl:"",src:"images/periodical/201306.jpg",text:"《NAK乐园》2013年06期"},
+	{pdfUrl:"",src:"images/periodical/201307.jpg",text:"《NAK乐园》2013年07期"},
+	{pdfUrl:"",src:"images/periodical/201308.jpg",text:"《NAK乐园》2013年08期"},
+	{pdfUrl:"",src:"images/periodical/201309.jpg",text:"《NAK乐园》2013年09期"},
+	{pdfUrl:"",src:"images/periodical/201310.jpg",text:"《NAK乐园》2013年10期"},
+	{pdfUrl:"",src:"images/periodical/201311.jpg",text:"《NAK乐园》2013年11期"},
+	{pdfUrl:"",src:"images/periodical/201312.jpg",text:"《NAK乐园》2013年12期"},
+	{pdfUrl:"",src:"images/periodical/201313.jpg",text:"《NAK乐园》2013年13期"}
 ]
 
 
